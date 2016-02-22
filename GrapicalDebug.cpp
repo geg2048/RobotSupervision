fcaa94875dc27a92ca28fec99ef9bb89015ae07b
geg2048 <geg2048@users.noreply.github.com>
2016-02-22 01:05:02 +0100

    exlude updateHSV

--- GrapicalDebug.cpp
+++ GrapicalDebug.cpp
@@ -5,9 +5,11 @@
 RobotDetection *gRobDec;
 cv::Mat gIMG;
 
+/*
 void updateHSV(int s,void *vp){
 	HSV_FILTER hsv = *((HSV_FILTER*)(vp));
 	gRobDec->setHSV(hsv.iLowH, hsv.iHighH, hsv.iLowS, hsv.iHighS, hsv.iLowV,hsv.iHighV);
 }
 
+*/
 #endif
